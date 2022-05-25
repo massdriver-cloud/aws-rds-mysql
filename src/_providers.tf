6eb556889ed8de8169a01b947259d024e412445c
Cory O'Daniel <cory@coryodaniel.com>
2022-05-25 12:23:43 -0700

    aws region from network

--- src/_providers.tf
+++ src/_providers.tf
@@ -13,7 +13,7 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
+  region = var.network.specs.aws.region
   assume_role {
     role_arn    = var.aws_authentication.data.arn
     external_id = var.aws_authentication.data.external_id
